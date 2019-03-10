a58481f7d0052ee8e75ce03b5a412d5194c9ab0b
moisesoda <moisesguedesah@gmail.com>
2019-03-10 20:27:06 -0300

    Projeto Cred Utilizando Table Jframe

---
+++ src/creedtable/CreedTable.java
@@ -0,0 +1,14 @@
+
+package creedtable;
+
+import view.FrameCred;
+
+public class CreedTable {
+
+    public static void main(String[] args) {
+        FrameCred tela = new FrameCred();
+        tela.setVisible(true);
+
+    }
+    
+}
